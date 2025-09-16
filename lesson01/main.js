8a4b2fdee422cf1bfa2419a0dd2b5065d7a695f5
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-16 17:49:13 +0700

    scope

--- lesson01/main.js
+++ lesson01/main.js
@@ -1,1 +1,41 @@
-console.log("Lập trình JS Nâng cao");
+// console.log("Lập trình JS Nâng cao");
+
+// console.log(myName); // undefined
+// console.log(myAge); // Lỗi
+
+var myName = "Chinhpd5";
+let myAge = 20;
+const myGender = true;
+
+// sayHello(); // hositing
+
+function sayHello() {
+  console.log("abc");
+}
+
+// global scope
+let a = 1; // var và let đều có phạm vi toàn cục
+
+{
+  a = 2;
+  // console.log(a);
+}
+
+// console.log(a);
+
+// block scope
+{
+  let b = 1; // let chỉ truy cập trong 1 khối (block), var thì có thể
+  // console.log(b);
+}
+
+// console.log(b);
+
+// function scope
+function sayHello1() {
+  let c = 1; // cả var và let chỉ có thể truy cập trong function scope
+}
+
+// console.log(c);
+
+
