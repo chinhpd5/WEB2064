6c18d44d64a32a6e2e66bb30ce841e345513de2e
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-20 19:55:46 +0700

    if else

---
+++ lession2/main.js
@@ -0,0 +1,25 @@
+// toán tử
+// toán tử logic
+
+let mark = 7;
+
+if(mark >= 0 && mark < 5){
+  console.log("Yếu");
+}else if(mark >= 5 && mark < 7){
+  console.log("Trung bình");
+}
+else if(mark >= 7 && mark < 9){
+  console.log("Khá");
+}else{
+  console.log("Giỏi");
+}
+
+if(mark < 5){
+  console.log("Yếu");
+}else if(mark < 7){
+  console.log("Trung bình");
+}else if(mark < 9){
+  console.log("Khá");
+}else{
+  console.log("Giỏi");
+}
