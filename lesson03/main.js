0645cfd8923e3bd35f0c244dcb512c5331d7fd15
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-21 10:25:10 +0700

    destructring

--- lesson03/main.js
+++ lesson03/main.js
@@ -55,6 +55,36 @@ var sayHello3 = (a) => {
 
 // cú pháp rút gọn của arrow function
 var sayHello4 = a => a+2;
-console.log(sayHello4(2));
+// console.log(sayHello4(2));
+
+// destructring
+// mảng
+var [a, , ,d] = [1,2,3,4];
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+// console.log(d);
+
+// object
+var { name, age, child: {name : childName, home} } = {
+  name: 'chinhpd5',
+  age: 20,
+  child: {
+    name: 'chinhpd6',
+    home: 'Hà Nội'
+  }
+}
+
+console.log(name);
+console.log(age);
+console.log(childName);
+console.log(home);
 
 
+
+
+
+
+// rest
+
+// spread
