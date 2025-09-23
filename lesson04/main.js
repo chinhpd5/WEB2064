404b455b0bb70f669518d7e0ed18be264caa62a3
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-23 19:27:40 +0700

    querySelector và querySeletorAll

--- lesson04/main.js
+++ lesson04/main.js
@@ -9,7 +9,20 @@ const pElements = document.getElementsByClassName('paragraph');
 // console.log(pElements); // trả về 1 HTMLCollection: Mảng chứa các phần tử
 
 const pTagElement = document.getElementsByTagName('p');
-console.log(pTagElement); // trả về 1 HTMLCollection: Mảng chứa các phần tử
-
+// console.log(pTagElement); // trả về 1 HTMLCollection: Mảng chứa các phần tử
 
 // css selector
+
+// trả về 1 element đầu tiên tìm thấy
+// console.log(document.querySelector('#title')); // id
+// console.log(document.querySelector('.paragraph')); //class
+// console.log(document.querySelector('p')); // tag
+// console.log(document.querySelector('#parent .paragraph'));
+
+
+// trả về NodeList: Mảng chứa các phần tử
+console.log(document.querySelectorAll('.paragraph'));
+
+
+
+
