65b3854fc007becfb3abf185ccd99ec89fc45241
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-25 19:49:06 +0700

    text

--- lesson05/main.js
+++ lesson05/main.js
@@ -5,10 +5,10 @@ const imgElement = document.querySelector('img');
 // console.log(imgElement);
 
 // truy xuất
-console.log(imgElement.src);
-console.log(imgElement.alt);
-console.log(imgElement.className);
-console.log(imgElement.getAttribute('width'));
+// console.log(imgElement.src);
+// console.log(imgElement.alt);
+// console.log(imgElement.className);
+// console.log(imgElement.getAttribute('width'));
 
 // gán - thêm
 imgElement.src = 'https://cdn-media.sforum.vn/storage/app/media/anh-dep-68.jpg'
@@ -19,4 +19,15 @@ imgElement.setAttribute('abc','chinhpd5')
 
 // xóa
 imgElement.removeAttribute('abc')
+
+// Text
+const h1Elment = document.querySelector('h1');
+
+console.log(h1Elment.textContent);
+h1Elment.textContent = 'Chào mừng bạn đến với JavaScript'
+
+const divElement = document.getElementById('content');
+divElement.innerHTML = '<h2>Đây là thẻ h2</h2>'
+
+
 
