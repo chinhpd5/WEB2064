a81d659f1d29843c4191b8808b010f8a603034d9
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-09-27 16:56:49 +0700

    find

--- lesson06/main.js
+++ lesson06/main.js
@@ -8,12 +8,30 @@ const students = [
   { name: "Long", age: 21, gender: true, mark: 6.8 },
   { name: "Trang", age: 19, gender: false, mark: 9.2 },
   { name: "Minh", age: 22, gender: true, mark: 7.4 },
-  { name: "Hương", age: 20, gender: false, mark: 3.9 }
+  { name: "Hương", age: 19, gender: false, mark: 3.9 }
 ];
+console.log(students);
+
 
 // forEach
-students.forEach((item,index,array) => {
-  console.log(item); // giá trị
-  console.log(index); // vị trí
-  // console.log(array); // mảng cũ
-})
+// students.forEach((item,index,array) => {
+//   // console.log(item); // giá trị
+//   // console.log(index); // vị trí
+//   // console.log(array); // mảng cũ
+//   if(item.mark >= 8 ){
+//     console.log(item.name + ", Học lực Giỏi");
+//   }else if(item.mark >=5){
+//     console.log(item.name + ", Học lực Khá");
+//   }else{
+//     console.log(item.name + ", Học lực Yếu");
+//   }
+// })
+
+// find
+const findItem = students.find((item,index) => {
+  // return item.name == "Trang"
+  console.log(index);
+  return item.age < 20
+});
+
+console.log(findItem);
