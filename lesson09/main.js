07a76c943bcf7666fb21d28abd74ae2fc9550e92
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-10-04 20:02:27 +0700

    async await

--- lesson09/main.js
+++ lesson09/main.js
@@ -71,11 +71,11 @@ const doingPromise = () => {
   delayPromise(2000)
     .then((data)=>{
       console.log(data); 
-      return delayPromise(1500)
+      return delayPromise(1500); // thực thi trong .then tiếp theo
     })
     .then(data => {
       console.log(data);
-      return delayPromise(1000)
+      return delayPromise(1000); // nếu xảy ra lỗi sẽ vào .catch
     })
     .then(data=>{
       console.log(data);
@@ -85,4 +85,59 @@ const doingPromise = () => {
     })
 }
 
-doingPromise();
+// doingPromise();
+
+// async - await
+
+const doingAsync = async () => {
+  try {
+    console.log(1);
+    console.log(await delayPromise(2000));
+    console.log(3);
+    console.log(await delayPromise(1500));
+    console.log(await delayPromise(1000));
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+// doingAsync()
+// async function test(){}
+
+const getData = async () => {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    // console.log(response);
+    const data = await response.json();
+    console.log(data);
+    renderTable(data)
+  } catch (error) {
+    alert(error)
+  }
+
+  // fetch(`https://jsonplaceholder.typicode.com/users`)
+  //   .then(res =>{
+  //     // console.log(res);
+  //     return res.json();
+  //   })
+  //   .then(data=>{
+  //     console.log(data);
+  //     renderTable(data)
+  //   })
+  //   .catch(err=>{
+  //     alert(err)
+  //   })
+}
+
+getData();
+
+const renderTable = (list)=>{
+  const liList = list.map((item,index)=>{
+    return `<li>${index+1} | ${item.name} | ${item.phone}</li>`
+  }).join('')
+
+  // console.log(liList);
+  const ulElement = document.querySelector('ul');
+  ulElement.innerHTML = liList
+}
